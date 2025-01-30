7bfc64bcda976d4bc94a4e368f3322f4a5f68355
Maija Lehtosaari <lehtosaarimaija@gmail.com>
2025-01-30 19:29:04 +0200

    State: works with 2 arguments, prints to terminal without reversing

--- project1/reverse.c
+++ project1/reverse.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #define MAX_LENGHT 1000 
 // argc numbers of arguments, argv array of arguments, argv[0] = ./reverse and argv[1] is input.txt
 
-int main (int argc, char *argv[]) {
-
-    FILE *inputFile = fopen(argv[1], "r");
+int readAndPrint(FILE *inputFile, FILE *outputFile) {
     char oneLine[MAX_LENGHT];
     while (fgets(oneLine, sizeof(oneLine), inputFile)) {
         fprintf(stdout, "%s", oneLine);
     }
-    fclose(inputFile);
+}
+int main (int argc, char *argv[]) {
+    if (argc == 2) {
+        FILE *inputFile = fopen(argv[1], "r");
+
+        if (inputFile == NULL) {
+            fprintf(stderr, "error when opening inputfile '%s'\n", argv[1]);
+            return(1);
+        }
+        readAndPrint(inputFile, stdout);
+        fclose(inputFile);
+    }
+    printf("\n");
     return(0);
 }
 
