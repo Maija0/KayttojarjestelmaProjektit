a4d185deeb51f5690fbb6d31a8dd648a8a5986f2
Maija Lehtosaari <lehtosaarimaija@gmail.com>
2025-05-15 18:14:41 +0300

    Added mutex lock, changed comments

--- project3/punzip.c
+++ project3/punzip.c
@@ -1,18 +1,20 @@
+// This code was done by copying pzip.c and modifying parts so the sources are same as in pzip.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
 
 typedef struct {
-    const char *filename;
+    const char *filename; // Threads filename
 } ThreadA;
 
+pthread_mutex_t lock;  //Mutex used to sync stdout
 
+// This function compresses a file 
 void *punzip(void *arg) {
     ThreadA *targ = (ThreadA *)arg;
-    const char *filename = targ->filename;
 
-    FILE *file = fopen(filename, "r");
+    FILE *file = fopen(targ->filename, "r");
     if (file == NULL) {
         printf("punzip: cannot open file\n");
         exit(1);
@@ -22,9 +24,11 @@ void *punzip(void *arg) {
 
     while(fread(&count, sizeof(int), 1, file) == 1) { // converts 4byte binary back to integer
         fread(&character, sizeof(char), 1, file);   //reads the character and
+        pthread_mutex_lock(&lock); // This locks access so that no other thread can access this part of the code
         for (int i = 0; i < count; i++) { //Writes the character count-times
             putchar(character); 
         }
+        pthread_mutex_unlock(&lock); // Release the lock
     }
     fclose(file);
     return NULL;
@@ -32,25 +36,26 @@ void *punzip(void *arg) {
 
 int main (int argc, char *argv[]) {
     if (argc < 2) {
-        printf("unzip: file1 [file2 ...]\n");
+        printf("punzip: file1 [file2 ...]\n");
         exit(1);
     }
-
-    int file_count = argc-1; //updates the file count
+    // Updates the file count so that it can be used below in creating threads
+    int file_count = argc-1;
     pthread_t threads[file_count];
     ThreadA args[file_count];
 
-    //thread for each file 
+    // Create thread for each given file
     for (int i = 0; i < file_count; i++) {
         args[i].filename = argv[i + 1];
         if (pthread_create(&threads[i], NULL, punzip, &args[i]) != 0) {
-            fprintf(stderr, "unzip couldn't create thread\n");
+            fprintf(stderr, "punzip couldn't create thread\n");
             exit(1);
         }
     }
     for (int i = 0; i < file_count; ++i){
         pthread_join(threads[i], NULL);
     }
+    pthread_mutex_destroy(&lock);
     return(0);
 }
 
--- project3/pzip.c
+++ project3/pzip.c
@@ -1,20 +1,23 @@
 // https://www.geeksforgeeks.org/posix-threads-in-os/ 
 // https://pubs.opengroup.org/onlinepubs/7908799/xsh/pthread.h.html Lists all <phtread.h> functions such as join, create thread ...
 // https://en.wikipedia.org/wiki/Pthreads
+// https://www.geeksforgeeks.org/mutex-lock-for-linux-thread-synchronization/ I followed the example and surrounded the sections that could cause issues with lock/unlock
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
 
 typedef struct {
-    const char *filename;
+    const char *filename; // Threads filename
 } ThreadA;
 
+pthread_mutex_t lock;  //Mutex used to sync stdout
+
+// This function compresses a file 
 void *pzip(void *arg) {
     ThreadA *targ = (ThreadA *)arg;
-    const char *filename = targ->filename;
-    
-    FILE *file = fopen(filename, "r");
+    FILE *file = fopen(targ->filename, "r");
     if (file == NULL) {
         printf("pzip: cannot open file\n");
         exit(1);
@@ -31,15 +34,19 @@ void *pzip(void *arg) {
         if (current == prev) { //adds count until letter changes
             count++;
         } else { // letter changed so write the combination
+            pthread_mutex_lock(&lock); // This locks access so that no other thread can access this part of the code
             fwrite(&count, sizeof(int), 1, stdout); // use fwrite and write count as 4byte integer
             fwrite(&prev, sizeof(char), 1, stdout); // write the char
+            pthread_mutex_unlock(&lock); // Release the lock
             prev = current;
             count = 1;
         }
     }
     //writes the last bytes
+    pthread_mutex_lock(&lock);
     fwrite(&count, sizeof(int), 1, stdout);
     fwrite(&prev, sizeof(char), 1, stdout);
+    pthread_mutex_unlock(&lock);
     fclose(file);
 }
 
@@ -48,11 +55,12 @@ int main (int argc, char *argv[]) {
         printf("pzip: file1 [file2 ...]\n");
         exit(1);
     }
-    int file_count = argc-1; //updates the file count
+    // Updates the file count so that it can be used below in creating threads
+    int file_count = argc-1;
     pthread_t threads[file_count];
     ThreadA args[file_count];
 
-    //thread for each file 
+    // Create thread for each given file
     for (int i = 0; i < file_count; i++) {
         args[i].filename = argv[i + 1];
         if (pthread_create(&threads[i], NULL, pzip, &args[i]) != 0) {
@@ -63,5 +71,6 @@ int main (int argc, char *argv[]) {
     for (int i = 0; i < file_count; ++i){
         pthread_join(threads[i], NULL);
     }
+    pthread_mutex_destroy(&lock);
     return(0);
 }
